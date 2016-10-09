f2ba113470908b5fbf5216e2b48490b4f42f3af4
yangfan <yangfan@funguide.com.cn>
2016-10-09 23:08:30 +0800

    init commit to develop branch

---
+++ hello-world/hello-world/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  hello-world
+//
+//  Created by MissYoung on 16/10/9.
+//  Copyright © 2016年 Fanny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Hello GitHub");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
